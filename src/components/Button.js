4287be2df16072f639536a75466d6f0070f44f0a
Dean Nash <shibta007@gmail.com>
2022-10-09 08:24:24 +0300

    Project 14 fixes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,21 +2,23 @@ import React from "react";
 import "../blocks/Button.css";
 
 function Button(props) {
-	return (
-		<button
-			className={"button " + props.className}
-			onClick={props.onClick}
-			id={
-				props.disabled
-					? !props.overide
-						? "button_inactive"
-						: "button_inactive_page"
-					: ""
-			}
-		>
-			{props.children}
-		</button>
-	);
+  return (
+    <button
+      className={"button " + props.className}
+      type={props.type || "button"}
+      disabled={props.disabled}
+      onClick={props.onClick}
+      id={
+        props.disabled
+          ? !props.overide
+            ? "button_inactive"
+            : "button_inactive_page"
+          : ""
+      }
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
